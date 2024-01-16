fda111e37dd7bc42dc8bf31dce7797c15fe1d72f
Kaveh Vakili <vakilik7@gmail.com>
2024-01-15 21:29:30 -0500

    added more loops to complete data for missing airplane codesn

--- flight-deals-start/main.py
+++ flight-deals-start/main.py
@@ -19,5 +19,19 @@ sheet=data_manager.get_sheet()
 #checking for missing airport codes
 for city in sheet:
     if city["iataCode"]=="":
-        
+        query_result=flight_search.find_city_code(city["city"])
+        city["iataCode"] = query_result[0]["code"]
+        data_manager.update_entry(city)
 
+
+for city in sheet:
+    today=dt.datetime.now()
+    tomorrow=today+dt.timedelta(days=1)
+    six_months=today+dt.timedelta(days=(6*30))
+    flight=flight_search.find_flights(origin=ORIGIN_CODE,destination=city["iataCode"],date_from=tomorrow.strftime("%d/%m/%Y"),
+                                        date_to=six_months.strftime("%d/%m/%Y"))
+    
+    if flight.price <= city["lowestPrice"]:
+        # to have some feedback
+        print("Sending a notification.")
+        notification_manager.send_sms(flight)  
