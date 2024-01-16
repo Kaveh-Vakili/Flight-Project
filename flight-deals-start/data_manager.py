d52cea4e2ce03f890005223971604feed576badf
Kaveh Vakili <vakilik7@gmail.com>
2024-01-15 21:31:38 -0500

    included the notifcation manager class

--- flight-deals-start/data_manager.py
+++ flight-deals-start/data_manager.py
@@ -15,4 +15,19 @@ class DataManager:
         response=requests.get(url=self.sheet_url,headers=self.headers)
         # to know what that the response code is 
         response.raise_for_status()
-     r
+    print("Spreadsheet successfully loaded.")
+    return response.json()["prices"]
+
+    def update_entry(self, entry):
+        """Takes a row number as STR and updates the corresponding row in the spreadsheet."""
+        edit_url = f"{self.sheet_url}/{entry['id']}"
+        body = {
+            "price": {
+                "iataCode": entry["iataCode"]
+            }
+        }
+        response = requests.put(url=edit_url, json=body, headers=self.headers)
+        # also just raise an error
+        response.raise_for_status()
+        # also to have some feedback
+        print(f"Row {entry['id']} has been updated with code {entry['iataCode']}.")
