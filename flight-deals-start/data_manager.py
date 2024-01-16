2f73588f9238a005a3097f8b5776db01677e877e
Kaveh Vakili <vakilik7@gmail.com>
2024-01-15 21:16:36 -0500

    created flight data class to get all fields from data sheet

--- flight-deals-start/data_manager.py
+++ flight-deals-start/data_manager.py
@@ -1,3 +1,18 @@
+import requests
+import config
+SHEETY_API_URL = "https://api.sheety.co"
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    
+    def __init__(self):
+        self.sheet_url = SHEETY_API_URL + config.SHEET_URL
+        # this is the same for all operations
+        self.headers = {
+            "Authorization": f"Bearer {config.SHEETY_TOKEN}"
+        }
+        
+    def get_sheet(self):
+        response=requests.get(url=self.sheet_url,headers=self.headers)
+        # to know what that the response code is 
+        response.raise_for_status()
+     r
--- flight-deals-start/main.py
+++ flight-deals-start/main.py
@@ -1,1 +1,23 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+import datetime as dt
+import data_manager as dm
+import flight_search as fs
+import notification_manager as nm
+
+
+ORIGIN_CODE = "IAD"
+
+# initialization
+data_manager = dm.DataManager()
+flight_search = fs.FlightSearch()
+notification_manager = nm.NotificationManager()
+
+sheet=data_manager.get_sheet()
+
+
+#checking for missing airport codes
+for city in sheet:
+    if city["iataCode"]=="":
+        
+
